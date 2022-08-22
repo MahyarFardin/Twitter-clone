0a34bf82975c349a26e3d6832d327b4ffd4c98d2
MahyarFardin <mahyarfardinfar@gmail.com>
2022-08-22 06:08:44 +0430

    first configurations done

---
+++ twitter/pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <p className="text-lGray">Test</p>
+    </div>
+  )
+}
---
+++ twitter/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    colors: {
+      "blue":"#1DA1F2",
+      "black":"#14171A",
+      "dGray":"#657786",
+      "lGray":"#AAB8C2",
+      "elGray":"#E1E8ED",
+      "eelGray":"#F5F8FA",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
